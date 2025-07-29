9219df82c66159478fbaa469ed47e1eeb6993e02
Nikolaos Oulis <n.oulis@oss.gr>
2025-07-29 20:12:48 +0300

    Last Changes

--- common/models/Bus.go
+++ common/models/Bus.go
@@ -19,6 +19,26 @@ type BusCapacityDto struct {
 	Date_modify time.Time `json:"date_time"`
 }
 
+type Bus_Capacity_01 struct {
+	Bus_Id     int64     `json:"bus_id" gorm:"column:bus_id;uniqueIndex"`
+	Route_Id   int32     `json:"route_id" gorm:"column:route_id;uniqueIndex"`
+	Sdc_Code   int32     `json:"sdc_code" gorm:"column:sdc_code;uniqueIndex"`
+	Start_Time OpswTime  `json:"start_time" gorm:"column:start_time;uniqueIndex"`
+	Bus_Cap    int32     `json:"bus_cap" gorm:"column:bus_cap"`
+	Bus_Pass   int32     `json:"bus_pass" gorm:"column:bus_pass"`
+	Date_Time  time.Time `json:"date_time" gorm:"column:date_time"`
+}
+
+type BusCapacityDto01 struct {
+	Bus_Id      int64     `json:"bus_id"`
+	Route_Id    int32     `json:"route_id"`
+	SdcCode     int32     `json:"sdc_code"`
+	StartTime   OpswTime  `json:"start_time"`
+	Bus_Cap     int32     `json:"bus_cap"`
+	Passengers  int32     `json:"bus_pass"`
+	Date_modify time.Time `json:"date_time"`
+}
+
 type BusCapacityDt02 struct {
 	Bus_Cap     int32     `json:"bus_cap"`
 	Passengers  int32     `json:"bus_pass"`
--- webApplication/db/migrations/000004_create_bus_capacity_table.up.sql
+++ webApplication/db/migrations/000004_create_bus_capacity_table.up.sql
@@ -8,3 +8,14 @@ CREATE TABLE bus_capacity (
     PRIMARY KEY (`id`),
     UNIQUE KEY `BUS_ROUTE_INDX` (`bus_id`, `route_id`, `date_time`)
 );
+
+create table bus_capacity_01 (
+    bus_id int,
+    route_id int,
+    sdc_code int,
+    start_time time,
+    bus_cap int,
+    bus_pass int, 
+    date_time datetime,
+    UNIQUE KEY `BUS_CAP_INDX` (`bus_id`, `route_id`, `sdc_code`, `start_time`)
+);
